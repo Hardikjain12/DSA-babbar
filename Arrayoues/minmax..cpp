7e836f8cbbaa8948532484d3602a90e6e3d6051c
Hardikjain12 <Hardikj714@gmail.com>
2024-07-09 20:51:51 +0530

    new

---
+++ Arrayoues/minmax..cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int getMin(int num[],int n)
+{
+    int min = INT_MAX;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (num[i] < min)
+        {
+            min = num[i];
+        }  
+    }
+    return min;
+}
+int getMax(int num[],int n)
+{
+    int max = INT_MIN;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (num[i] > max)
+        {
+            max = num[i];
+        }  
+    }
+    return max;
+}
+int main()
+{
+    int size;
+    cin >> size;
+
+    int num[10000];
+
+    for (int i = 0; i < size; i++)
+    {
+        cin >> num[i];
+    }
+    
+    cout<<"MAXIMUM VALUE IS " <<getMax(num,size)<<endl;
+    cout<<"MINIMUM VALUE IS " <<getMin(num,size)<<endl;
+    return 0;
+}
